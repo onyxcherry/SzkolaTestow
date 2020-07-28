695ea7fbd6680d8152366ef56190b718bca6fa9f
Tomek <tomek@onyxcherry.pl>
2020-07-28 10:52:20 +0200

    Add handling spare in the last frame

--- ModuleThree/app/bowling.py
+++ ModuleThree/app/bowling.py
@@ -2,7 +2,7 @@ class Bowling:
   def __init__(self):
     self.score = 0
     self.pins = []
-    self.counter = 0
+    self.cursor = 0
 
   def roll(self, knocked_pins):
     self.pins.append(knocked_pins)
@@ -10,18 +10,29 @@ class Bowling:
 
   def get_score(self):
     self.score = sum(self.pins)
-    for x in range(20):
+    for x in range(10):
       if self.check_if_spare():
-        self.score += self.pins[self.counter + 2]
-        self.counter += 1
+        self.score += self.pins[self.cursor + 2]
+        self.cursor += 2
       elif self.check_if_strike():
-        self.score += self.pins[self.counter + 1]
-        self.score += self.pins[self.counter + 2]
-        self.counter += 1
+        self.score += self.pins[self.cursor + 1]
+        self.score += self.pins[self.cursor + 2]
+        self.cursor += 1
+      else:
+        self.cursor += 2
     return self.score
 
+  def check_if_range(self):
+    if self.cursor > len(self.pins) or self.cursor + 1 > len(self.pins):
+      return True
+    return False
+
   def check_if_spare(self):
-    return self.pins[self.counter] + self.pins[self.counter + 1] == 10
+    if self.check_if_range():
+      return False
+    return self.pins[self.cursor] + self.pins[self.cursor + 1] == 10
 
   def check_if_strike(self):
-    return self.pins[self.counter] == 10
+    if self.check_if_range():
+      return False
+    return self.pins[self.cursor] == 10
